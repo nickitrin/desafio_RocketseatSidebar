f2a32dcf0973ab99b43c4ca7a877b68cb4094f5e
Bernardo <bernardo.bt@gmail.com>
2021-11-29 12:19:49 -0300

    feat: percorrendo elementos no vetor com forEach.

--- app/js/script.js
+++ app/js/script.js
@@ -3,13 +3,22 @@ const Menu = {
     Close(){
         let $elementos_textos_menu = document.querySelectorAll(".textos_menu")
         
-        $elementos_textos_menu.map(elemento => {
-            console.log(elemento);
-            return 0
-        })
+        console.log($elementos_textos_menu)
+
+        $elementos_textos_menu.forEach(item => {
+            item.classList.add('nova classe')
+            console.log(item.innerHTML.trim())
+        } )
+
        
             
-        
+        /*
+            .map();
+            .find();
+            .findIndex();
+            .forEach();
+
+        */
     }
 
 
