f6d212468cd240d3d53c1144ad895d84d7ff139e
Nicole Trindade <nicole.trindade@poli.ufrj.br>
2021-11-29 12:21:17 -0300

    feat: alteração no codigo close

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,13 @@
 const Menu = {
 
     Close(){
-        let $elementos_textos_menu = document.querySelectorAll(".textos_menu")
-        
-        $elementos_textos_menu.map(elemento => {
-            console.log(elemento);
-            return 0
-        })
-       
-            
+        //let $elementos_textos_menu = 
+        let i=0;
+        for ( i<6 ) {
+        document.querySelector(".textos_menu").classList.remove("desativado");
+        i++;
+        console.log(i)
+    }
         
     }
-
-
 }
