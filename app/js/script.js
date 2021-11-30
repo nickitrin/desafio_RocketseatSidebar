cbd1f5a27458f36171955167353177ccd6cfb47b
Nicole Trindade <nicole.trindade@poli.ufrj.br>
2021-11-30 09:33:18 -0300

    feat: mostrar ou esconder menu + style:alinhando elementos

--- app/js/script.js
+++ app/js/script.js
@@ -1,23 +1,21 @@
 const Menu = {
 
     Close(){
-        let $elementos_textos_menu = document.querySelectorAll(".textos_menu")
-        
-        console.log($elementos_textos_menu)
+        /*pegando os elementos com as classes que preciso esconder, mostrar*/
+        let $elementos_textos_menu = document.querySelectorAll(".textos_menu,.info_user, .imagem_logo")
 
+        /*função que opera em cada elemento*/
         $elementos_textos_menu.forEach(item => {
-            item.classList.add('nova classe')
-            console.log(item.innerHTML.trim())
-        } )
+        /*método que adiciona ou remove alguma string das classes selecionadas*/
+            item.classList.toggle('desativado')
 
-       
-            
+        } )   
+
         /*
             .map();
             .find();
             .findIndex();
             .forEach();
-
         */
     }
 }
